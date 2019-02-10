fcd692f2f07d2b42f6e69a85ee3d085b649a25ac
OMARV\Omar <mohammad.omar.317@gmail.com>
2019-02-10 14:21:29 +0800

    Complete tutorial

---
+++ Text 101/Assets/Scripts/TextAdventure.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAdventure : MonoBehaviour
+{
+    [SerializeField] private Text storyTextComponent;
+    [SerializeField] private State startingState;
+
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        storyTextComponent.text = state.GetStateStory();
+    }
+
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextState = state.GetNextState();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (nextState[1] != null)
+            {
+                state = nextState[1];
+            }
+        }
+        storyTextComponent.text = state.GetStateStory();
+    }
+}
